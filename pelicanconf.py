488eca56b4bf08d31b3d3bc6a5a68005d31c7650
chiayolin <chiayo.lin@gmail.com>
2016-07-18 23:03:22 -0400

    import codehilite markdown extension

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,43 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
+from markdown.extensions.codehilite import CodeHiliteExtension
+from markdown.extensions.toc import TocExtension
 
+# site meta
 AUTHOR = u'Chiayo Lin'
 SITENAME = u'chiayolin.org'
 SITEURL = ''
 
+# import theme
 THEME = './lowlands'
 
+# paths
 PATH = 'content'
 STATIC_PATHS = ['etc/CNAME', 'etc/favicon.ico']
 EXTRA_PATH_METADATA = {'etc/CNAME': {'path': 'CNAME'}, 'etc/favicon.ico': {'path': 'favicon.ico'}} 
 
+# input/output path for htmls
+ARTICLE_URL = 'posts/{slug}/'
+ARTICLE_SAVE_AS = '{slug}/index.html'
 PAGE_URL = 'pages/{slug}/'
 PAGE_SAVE_AS = '{slug}/index.html'
 
+# time and date
 TIMEZONE = 'Asia/Taipei'
 DEFAULT_DATE_FORMAT = '%a %b %d %Y'
 
+# local language
 DEFAULT_LANG = u'en'
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+# import markdown extenstions
+MD_EXTENSIONS = [
+        CodeHiliteExtension(css_class='highlight'),
+        TocExtension(permalink=True),
+        'markdown.extensions.extra',
+]
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
+# disable pagenation
 DEFAULT_PAGINATION = False
-
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
