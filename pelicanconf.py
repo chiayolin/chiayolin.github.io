1d8cc1b76e700a44760414c118d0192b84ffeb2a
chiayolin <chiayo.lin@gmail.com>
2016-07-19 00:31:54 -0400

    update past projects

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,7 +19,7 @@ EXTRA_PATH_METADATA = {'etc/CNAME': {'path': 'CNAME'}, 'etc/favicon.ico': {'path
 
 # input/output path for htmls
 ARTICLE_URL = 'posts/{slug}/'
-ARTICLE_SAVE_AS = '{slug}/index.html'
+ARTICLE_SAVE_AS = '{slug}/'
 PAGE_URL = 'pages/{slug}/'
 PAGE_SAVE_AS = '{slug}/index.html'
 
