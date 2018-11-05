2cc5a625915b43681244c62427b457dd6b5fa861
chiayolin <chiayo.lin@gmail.com>
2018-11-05 18:47:42 -0500

    update deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,46 +1,54 @@
 #!/usr/bin/env bash
 # source: http://iamemmanouil.com/blog/\
 #         static-site-pelican-grunt-travis-github-pages/#deploy
-BRANCH=master
-TARGET_REPO=chiayolin/chiayolin.github.io.git
-OUTPUT_FOLDER=_output
 
-echo -e "testing travis-encrypt"
-echo -e "$VARNAME"
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+BRANCH="master"
+OUTPUT="_output"
+NAME="Chiayo Lin"
+MAIL="chiayo.lin@gmail.com"
+REPO="chiayolin/chiayolin.github.io.git"
 
-    if [ "$TRAVIS" == "true" ]; then
-        git config --global user.email "chiayo.lin@gmail.com"
-        git config --global user.name "Chiayo Lin"
+echo "testing travis-encrypt"
+echo "$VARNAME"
+
+if test "$TRAVIS_PULL_REQUEST" = "false"; then
+
+    if test "$TRAVIS" = "true"; then
+        git config --global user.email $MAIL
+        git config --global user.name  $NAME
     fi
 
-    # Using token clone gh-pages branch
-    git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null
+    # using api token clone gh-pages branch
+    git clone --quiet                                               \
+		--branch=$BRANCH https://${GH_TOKEN}@github.com/$REPO built \
+			> /dev/null
 
-    # Go into directory and copy data we're interested in to that directory
-    cd built_website
-    rsync -rv --exclude=.git  ../$OUTPUT_FOLDER/* .
+	# since the site is built, cd into that directory and rync target
+    cd built
+    rsync -rv --exclude=.git  ../$OUTPUT/* .
 
-    echo -e "remove previous version of website\n"
+    echo "remove previous version of website"
     git rm -rf .
     git clean -f -d
     git commit -m "empty the branch before pushing($TRAVIS_BUILD_NUMBER)"
     git push -fq origin $BRANCH > /dev/null
     cd ..
 
-    echo -e "starting deployment on Github Pages\n"
-    # Using token clone gh-pages branch
-    git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null
+    echo "starting deployment on Github Pages"
+    # using api token clone gh-pages branch
+    git clone --quiet                                               \
+		--branch=$BRANCH https://${GH_TOKEN}@github.com/$REPO built \
+			> /dev/null
 
-    # Go into directory and copy data we're interested in to that directory
-    cd built_website
-    rsync -rv --exclude=.git  ../$OUTPUT_FOLDER/* .
+    # rync target to the gh-pages branch
+    cd built
+    rsync -rv --exclude=.git  ../$OUTPUT/* .
 
-    # Add, commit and push files
+    # add, commit and push files
     git add -f .
     git commit -m "travis build $TRAVIS_BUILD_NUMBER pushed to Github Pages"
     git push -fq origin $BRANCH > /dev/null
 
-    echo -e "deploy completed\n"
+    echo -e "site deployed\n"
 fi
