89cb9e52d77c496d26e08f974a061ec511997d53
chiayolin <chiayo.lin@gmail.com>
2020-12-31 11:50:58 +0700

    update test script

--- test.sh
+++ test.sh
@@ -2,6 +2,12 @@
 #
 # $ sh test.sh
 
+# install requirements
+pip3 install -r requirements.txt
+
+# render the site
 python3 render.py
+
+# serve rendered web pages
 cd _output
 python3 -m http.server && cd ..
