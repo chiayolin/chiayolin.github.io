8879393d2cea407104ba8e6c9d16bb07de89c1f4
chiayolin <chiayo.lin@gmail.com>
2021-01-03 18:12:14 -0800

    fix tabs/spaces

--- deploy.sh
+++ deploy.sh
@@ -13,43 +13,43 @@ echo "$VARNAME"
 
 if test "$TRAVIS_PULL_REQUEST" = "false"; then
 
-    if test "$TRAVIS" = "true"; then
-        git config --global user.email $MAIL
-        git config --global user.name  $NAME
-    fi
+  if test "$TRAVIS" = "true"; then
+    git config --global user.email $MAIL
+    git config --global user.name  $NAME
+  fi
 
-    # using api token clone gh-pages branch
-    git clone                                                     \
-      --quiet                                                     \
-      --branch=$BRANCH https://${GH_TOKEN}@github.com/$REPO built \
-        > /dev/null
+  # using api token clone gh-pages branch
+  git clone                                                     \
+    --quiet                                                     \
+    --branch=$BRANCH https://${GH_TOKEN}@github.com/$REPO built \
+      > /dev/null
 
   # since the site is built, cd into that directory and rync target
-    cd built
-    rsync -rv --exclude=.git  ../$OUTPUT/* .
+  cd built
+  rsync -rv --exclude=.git  ../$OUTPUT/* .
 
-    echo "remove previous version of website"
-    git rm -rf .
-    git clean -f -d
-    git commit -m "empty the branch before pushing($TRAVIS_BUILD_NUMBER)"
-    git push -fq origin $BRANCH > /dev/null
-    cd ..
+  echo "remove previous version of website"
+  git rm -rf .
+  git clean -f -d
+  git commit -m "empty the branch before pushing($TRAVIS_BUILD_NUMBER)"
+  git push -fq origin $BRANCH > /dev/null
+  cd ..
 
-    echo "deploying to Github Pages"
-    # using api token clone gh-pages branch
-    git clone                                                     \
-      --quiet                                                     \
-      --branch=$BRANCH https://${GH_TOKEN}@github.com/$REPO built \
-        > /dev/null
+  echo "deploying to Github Pages"
+  # using api token clone gh-pages branch
+  git clone                                                     \
+    --quiet                                                     \
+    --branch=$BRANCH https://${GH_TOKEN}@github.com/$REPO built \
+      > /dev/null
 
-    # rync target to the gh-pages branch
-    cd built
-    rsync -rv --exclude=.git  ../$OUTPUT/* .
+  # rync target to the gh-pages branch
+  cd built
+  rsync -rv --exclude=.git  ../$OUTPUT/* .
 
-    # add, commit and push files
-    git add -f .
-    git commit -m "travis build $TRAVIS_BUILD_NUMBER pushed to Github Pages"
-    git push -fq origin $BRANCH > /dev/null
+  # add, commit and push files
+  git add -f .
+  git commit -m "travis build $TRAVIS_BUILD_NUMBER pushed to Github Pages"
+  git push -fq origin $BRANCH > /dev/null
 
-    echo -e "site deployed\n"
+  echo -e "site deployed\n"
 fi
